6ed441ec2d8a2fcab74dbbaf6812b93bb6cfbaf5
GorBabloyan <104680741+GorBabloyan@users.noreply.github.com>
2024-10-08 13:04:16 +0400

    changesNote

--- JavaDataStructuresAlgoritms/src/test/java/com/SortArr.java
+++ JavaDataStructuresAlgoritms/src/test/java/com/SortArr.java
@@ -2,13 +2,26 @@ package com;
 
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class SortArr extends Base {
 
 
-
+    static {
+        System.out.println("a");
+    }
+    {
+        System.out.println("b");
+    }
+    public SortArr(){
+        System.out.println("c");
+    }
+    public void sortArrMethod(){
+        System.out.println("d");
+    }
 
     @Test
     public void sortArrayJava(){
@@ -39,7 +52,12 @@ public class SortArr extends Base {
     @Test
     public void test(){
         int[] numbers = {10,3,8,78,9,1};
+        List<Integer> numbersList = new ArrayList<>();
+        for (int n : numbers){
+            numbersList.add(n);
+        }
 
+        Collections.sort(numbersList);
         int[] sortedNumbers = sortArrayInt(numbers);
 
         for (int i=0;i<sortedNumbers.length;i++){
